da4e4bc924c76d3ab50bc7e5911caebd547824fa
Alan Janis <alan.janis@gmail.com>
2020-06-23 16:23:29 -0600

    start of transforms work

--- src/python-redfish-raid/domain/usecases/api/FilterOutputUseCase.py
+++ src/python-redfish-raid/domain/usecases/api/FilterOutputUseCase.py
@@ -1,6 +1,20 @@
 import jmespath
+import humanize
+
+from string import Formatter
 
 from framework.usecases.UseCase import UseCase
+
+
+class KWArgFormatter(Formatter):
+    def get_value(self, key, args, kwargs):
+        if isinstance(key, str):
+            try:
+                return kwargs[key]
+            except KeyError:
+                return key
+        else:
+            return Formatter.get_value(key, args, kwargs)
 
 
 class FilterOutputCaseUseCase(UseCase):
@@ -10,6 +24,7 @@ class FilterOutputCaseUseCase(UseCase):
         """Create a use case for performing queries against the JSON responses."""
         super().__init__()
         self._json_filters = json_filters
+        self._keyword_formatter = KWArgFormatter()
 
     def __call__(self, service_data_json):
         """Perform Json Query/Select operations against the API responses."""
@@ -17,5 +32,19 @@ class FilterOutputCaseUseCase(UseCase):
             return None
         response = {}
         for query in self._json_filters:
-            response[query['key']] = jmespath.search(query['jq'], service_data_json)
+            key = query['key']
+            filtered_json = jmespath.search(query['jq'], service_data_json)
+            # print(filtered_json)
+            # if 'transforms' in query:
+            #     transforms = query[trans]
+            #     for item in filtered_json:
+            #         if item not in transforms:
+            #             filtered_json
+            #     filtered_json = [item if item not in transforms else humanize.__getattribute__(transforms[item])(item) for item in filtered_json]
+
+            if 'format' in query:
+                item_format = query['format']
+                response[key] = ''.join([self._keyword_formatter.format(item_format, **item) for item in filtered_json])
+            else:
+                response[key] = filtered_json
         return response
