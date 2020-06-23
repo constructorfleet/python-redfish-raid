9d24c74dc9dde6cc91f53d221831407ba2ffb50a
Teagan glenn <teagan.m.glenn@gmail.com>
2020-06-23 17:38:32 -0600

    Substring

--- src/python-redfish-raid/domain/usecases/api/FilterOutputUseCase.py
+++ src/python-redfish-raid/domain/usecases/api/FilterOutputUseCase.py
@@ -1,5 +1,5 @@
 from string import Formatter
-
+import math
 import humanize
 import jmespath
 from jmespath import functions
@@ -30,6 +30,21 @@ class CustomFunctions(functions.Functions):
     @functions.signature({'types': ['number']})
     def _func_scientific(self, value):
         return humanize.scientific(value)
+
+    @functions.signature({'types': ['string']}, {'types': ['number']}, {'types': ['number']})
+    def _func_substring(self, value, start_index=None, end_index=None):
+        if start_index and end_index:
+            start_index = min(0, int(start_index))
+            end_index = min(len(value) - 1, int(end_index))
+            return value[start_index:end_index]
+        elif start_index:
+            start_index = min(0, int(start_index))
+            return value[start_index:]
+        elif end_index:
+            end_index = min(len(value) - 1, int(end_index))
+            return value[:end_index]
+        else:
+            return value
 
 
 class FilterOutputCaseUseCase(UseCase):
